e04b959591e42f6cb9547f123149fd81c7cbae61
markkvdb <github@mvanderbroek.com>
2024-12-16 11:02:44 +0100

    Install linter and formatter

--- tests/test_client.py
+++ tests/test_client.py
@@ -7,20 +7,25 @@ from frisquet_api.client import FrisquetClient
 # Load environment variables from .env file
 load_dotenv()
 
+
 @pytest.fixture
 def client() -> FrisquetClient:
     email = os.getenv("FRISQUET_EMAIL")
     password = os.getenv("FRISQUET_PASSWORD")
-    
+
     if not email or not password:
-        pytest.skip("FRISQUET_EMAIL and FRISQUET_PASSWORD environment variables are required")
-    
+        pytest.skip(
+            "FRISQUET_EMAIL and FRISQUET_PASSWORD environment variables are required"
+        )
+
     return FrisquetClient(email=email, password=password)
+
 
 async def test_get_token_fake_credentials(client: FrisquetClient):
     client = FrisquetClient(email="test@test.com", password="test")
     with pytest.raises(httpx.HTTPStatusError):
         await client.get_authentication()
+
 
 async def test_get_token_valid_credentials(client: FrisquetClient):
     token = await client.token
@@ -29,4 +34,4 @@ async def test_get_token_valid_credentials(client: FrisquetClient):
 
 async def test_get_site_data(client: FrisquetClient):
     site_data = await client.get_site_data("23425231180423")
-    x = 0
+    assert site_data is not None
